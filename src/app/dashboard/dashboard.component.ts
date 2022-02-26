b06ebf256eca77a61aa7c857a03708d1e14ef820
davidoludolapo <davidoluwadolapo@gmail.com>
2022-02-26 14:36:18 +0100

    worked on todo

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskInterface } from '../type/task.interface';
 import { UserService } from '../user.service';
 
 @Component({
@@ -7,6 +8,12 @@ import { UserService } from '../user.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  isDisabled = false;
+  public name: string = ''
+  public allTasks:Array<TaskInterface>=[]
+  public editData:boolean= false;
+  public editedIndex:number= 0
+
   public image:  string = "../assets/zenith.png"
   public image1: string = "../assets/gtb.png"
   public image2: string = "../assets/firstbank.png"
@@ -20,6 +27,38 @@ export class DashboardComponent implements OnInit {
     this.user = JSON.parse(localStorage["currentUser"])
     console.log(this.user.accountNo);
 
+  }
+
+  addTask(e:any) {
+    e.preventDefault()
+    let name = this.name
+    this.allTasks.push({name})
+    this.name= ''
+   
+  }
+
+  deleteTask(i:number){
+    this.allTasks =this.allTasks.filter((_,index)=>index!==i)
+    
+  }
+ 
+  editTask(i:number){
+   this.editData = true;
+   this.editedIndex= i;
+ 
+   let editedObject = this.allTasks[i]
+   this.name = editedObject.name
+   
+  }
+ 
+  saveChanges() {
+   let {name, editedIndex} = this
+   let editedObject = {name}
+   this.allTasks[editedIndex] = editedObject
+   this.editData =false
+   
+   this.name= ''
+   
   }
 
   logOut(){
